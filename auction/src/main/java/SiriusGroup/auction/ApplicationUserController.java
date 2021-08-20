b4edccab859d3d6681be05e2acfb026e97bf887d
abuzahra98 <dawoodbasher@gmail.com>
2021-08-20 14:58:12 +0300

    user

---
+++ auction/src/main/java/SiriusGroup/auction/ApplicationUserController.java
@@ -0,0 +1,51 @@
+package SiriusGroup.auction;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.xml.crypto.Data;
+
+
+@Controller
+public class ApplicationUserController {
+
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+
+    @GetMapping("/signup")
+    public String getSignUpPage(){
+        return "signup.html";
+    }
+
+
+    @GetMapping("/login")
+    public String getSignInPage(){
+        return "login.html";
+    }
+
+    @PostMapping("/signup")
+    public RedirectView signUp(@RequestParam(value="username") String username, @RequestParam(value="password") String password, @RequestParam(value="firstName") String firstName, @RequestParam(value="lastName") String lastName, @RequestParam(value="dateOfBirth") Integer dateOfBirth, @RequestParam(value="bio") String bio, @RequestParam(required=true,value="imageUrl") String imageUrl){
+        ApplicationUser newUser = new ApplicationUser(username,bCryptPasswordEncoder.encode(password),firstName,lastName,dateOfBirth,bio,imageUrl);
+        applicationUserRepository.save(newUser);
+        return new RedirectView("/login");
+    }
+
+
+
+
+
+}
---
+++ auction/src/main/java/SiriusGroup/auction/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package SiriusGroup.auction;
+
+import SiriusGroup.auction.ApplicationUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationUserRepository extends CrudRepository<ApplicationUser, Long> {
+    public ApplicationUser findByUsername(String username);
+}
