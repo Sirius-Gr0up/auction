813f6cb560e085d3da559669d83752e478aee77b
Mohammadabufarweh <abufarweh34@gmail.com>
2021-08-22 15:14:40 +0300

    productpage

--- auction/src/main/java/SiriusGroup/auction/ApplicationUserController.java
+++ auction/src/main/java/SiriusGroup/auction/ApplicationUserController.java
@@ -11,10 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -23,6 +20,7 @@ import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 @Controller
@@ -33,55 +31,53 @@ public class ApplicationUserController {
     ApplicationUserRepository applicationUserRepository;
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
-
+    @Autowired
+    ProductsRepository productsRepository;
 
 
     @GetMapping("/")
-    public String getHome(Principal p, Model model){
-        try{
-
-
-            model.addAttribute("UserInfo",applicationUserRepository.findById(applicationUserRepository.findByUsername(p.getName()).getId()).get());
+    public String getHome(Principal p, Model model) {
+        try {
+            model.addAttribute("UserInfo", applicationUserRepository.findById(applicationUserRepository.findByUsername(p.getName()).getId()).get());
             System.out.println(applicationUserRepository.findByUsername(p.getName()).getImgUrl());
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
 
-            model.addAttribute("userInfo","");
+            model.addAttribute("userInfo", "");
 
         }
         return "index.html";
     }
 
     @GetMapping("/signup")
-    public String getSignUpPage(){
+    public String getSignUpPage() {
         return "signup.html";
     }
 
 
     @GetMapping("/login")
-    public String getSignInPage(){
+    public String getSignInPage() {
         return "login.html";
     }
 
     @PostMapping("/signup")
-    public RedirectView signUp(@RequestParam(value="username") String username, @RequestParam(value="password") String password, @RequestParam(value="firstName") String firstName, @RequestParam(value="lastName") String lastName, @RequestParam(value="dateOfBirth") String dateOfBirth, @RequestParam(value="bio") String bio, @RequestParam(required=true,value="imageUrl") MultipartFile imageUrl) throws IOException {
+    public RedirectView signUp(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password, @RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName, @RequestParam(value = "dateOfBirth") String dateOfBirth, @RequestParam(value = "bio") String bio, @RequestParam(required = true, value = "imageUrl") MultipartFile imageUrl) throws IOException {
 //        ApplicationUser newUser = new ApplicationUser(username,bCryptPasswordEncoder.encode(password),firstName,lastName,dateOfBirth,bio,imageUrl);
 //        applicationUserRepository.save(newUser);
         //Ensure that user registration also logs users into your app automatically.
 
 
-
         String fileName = StringUtils.cleanPath(imageUrl.getOriginalFilename());
 
 
+        String uploadDir = "Users/S4C/auction/auction/src/main/resources/static/img";
 
-
-        String uploadDir = "/Users/dawoodabuzahra/auction/auction/src/main/resources/static/img" ;
+//        String uploadDir = "/Users/dawoodabuzahra/auction/auction/src/main/resources/static/img" ;
 //        String uploadDir = "/Users/user/LTUC/auction/auction/src/main/resources/static/img" ;
 
-       String url= FileUploadUtil.saveFile(uploadDir, fileName, imageUrl);
+        String url = FileUploadUtil.saveFile(uploadDir, fileName, imageUrl);
 
 
-        ApplicationUser newUser = new ApplicationUser(username,bCryptPasswordEncoder.encode(password),firstName,lastName,dateOfBirth,bio,url);
+        ApplicationUser newUser = new ApplicationUser(username, bCryptPasswordEncoder.encode(password), firstName, lastName, dateOfBirth, bio, url);
         applicationUserRepository.save(newUser);
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
@@ -91,29 +87,22 @@ public class ApplicationUserController {
 
 
     @GetMapping("/profile")
-    public String getUserProfile(Principal p, Model model){
-        try{
+    public String getUserProfile(Principal p, Model model) {
+        try {
             // for the header
 
             // for the body
-            model.addAttribute("UserInfo",applicationUserRepository.findById(applicationUserRepository.findByUsername(p.getName()).getId()).get());
+            model.addAttribute("UserInfo", applicationUserRepository.findById(applicationUserRepository.findByUsername(p.getName()).getId()).get());
             System.out.println(applicationUserRepository.findById(applicationUserRepository.findByUsername(p.getName()).getId()).get());
-        }catch (NullPointerException e){
-            model.addAttribute("userInfoe","");
-            model.addAttribute("UserInfo",new ApplicationUser());
+        } catch (NullPointerException e) {
+            model.addAttribute("userInfoe", "");
+            model.addAttribute("UserInfo", new ApplicationUser());
         }
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass()))
+        if (authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass()))
             return "login.html";
         else
             return "profile.html";
-    }
-
-    @GetMapping("/product" )
-    public String getProduct(Principal p, Model model){
-        model.addAttribute("UserInfo",applicationUserRepository.findById(applicationUserRepository.findByUsername(p.getName()).getId()).get());
-
-        return "product.html";
     }
 
 }
