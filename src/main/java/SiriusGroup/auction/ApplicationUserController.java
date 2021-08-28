96841231d94ecc021fc87fcd1598c0d83ad99f72
abuzahra98 <dawoodbasher@gmail.com>
2021-08-28 16:05:29 +0300

    edit

--- src/main/java/SiriusGroup/auction/ApplicationUserController.java
+++ src/main/java/SiriusGroup/auction/ApplicationUserController.java
@@ -82,8 +82,8 @@ public class ApplicationUserController {
 
 //                    String uploadDir = "/Users/S4C/auction/auction/src/main/resources/static/img";
 //         String uploadDir = "/Users/Khalil/ASAC/auction/auction/src/main/resources/static/img" ;
-//            String uploadDir = "/Users/dawoodabuzahra/401/auction/auction/src/main/resources/static/img" ;
-                    String uploadDir = "/Users/user/LTUC/auction/auction/src/main/resources/static/img" ;
+            String uploadDir = "/Users/dawoodabuzahra/401/auction/auction/src/main/resources/static/img" ;
+//                    String uploadDir = "/Users/user/LTUC/auction/auction/src/main/resources/static/img" ;
 //                    String uploadDir = "/Users/user/LTUC/auction/auction/src/main/resources/static/img" ;
 
 //                    String uploadDir = "/Users/S4C/auction/auction/src/main/resources/static/img";
--- src/main/java/SiriusGroup/auction/ProductController.java
+++ src/main/java/SiriusGroup/auction/ProductController.java
@@ -67,8 +67,8 @@ public class ProductController {
 
 
         String fileName = StringUtils.cleanPath(productImageUrl.getOriginalFilename());
-        String uploadDir = "/Users/user/LTUC/auction/auction/src/main/resources/static/img" ;
-//        String uploadDir = "/Users/dawoodabuzahra/401/auction/auction/src/main/resources/static/img" ;
+//        String uploadDir = "/Users/user/LTUC/auction/auction/src/main/resources/static/img" ;
+        String uploadDir = "/Users/dawoodabuzahra/401/auction/auction/src/main/resources/static/img" ;
 //        String uploadDir = "/Users/Khalil/ASAC/auction/auction/src/main/resources/static/img";
 //        String uploadDir = "Users/S4C/auction/auction/src/main/resources/static/img";
 //        String uploadDir = "/Users/Khalil/ASAC/auction/auction/src/main/resources/static/img";
