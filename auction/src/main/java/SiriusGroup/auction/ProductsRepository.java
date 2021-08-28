c0b26a2d4411bcb5895e51c2e3c43d9bba005aab
RawanAlakhras <ralakhras36@gmail.com>
2021-08-28 09:50:35 +0300

    add

--- auction/src/main/java/SiriusGroup/auction/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package SiriusGroup.auction;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface ProductsRepository extends CrudRepository <Products,Long> {
-
-}
